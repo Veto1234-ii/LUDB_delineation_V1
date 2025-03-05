615148392edbc8d90e11321f6a46b82b662a98f1
ivan <ivan_somov@example.com>
2025-03-05 13:35:29 +0300

    ecg viewer

--- datasets/GUI/UI_binary_dataset.py
+++ datasets/GUI/UI_binary_dataset.py
@@ -1,10 +1,15 @@
 from datasets.binary_datasets import BinaryDataset
+from GUI.ecg_viewer import ECGViewer  # подключение интерфейса
+import tkinter as tk
 
-import numpy as np
-
-# Листалка примеров из датасета. Нужна, чтобы зрительно ознакомиться с собранным датасетом.
 class UIBinaryDataset:
     def __init__(self, binary_dataset):
         train_data = binary_dataset.get_train()
-        #TODO
 
+        signals = train_data["signals"]  # проверить, как хранятся сигналы
+        answers = train_data["answers"]  # метки (0 или 1)
+
+        # запуск GUI
+        root = tk.Tk()
+        app = ECGViewer(root, signals, answers)
+        root.mainloop()
