8bdd0c7ecee3b6773216f44990b8b740cf4c5f13
Julia <kurdina.00@gmail.com>
2025-03-24 21:39:19 +0300

    load best net

--- neural_networks/__init__.py
+++ neural_networks/__init__.py
@@ -1,2 +1,3 @@
 from neural_networks.neural_networks_helpers.load_network import get_appliable
 from neural_networks.neural_networks_models.CNN import CNN
+from load_best_net import load_best_net
---
+++ neural_networks/load_best_net.py
@@ -0,0 +1,33 @@
+import os
+
+import torch
+
+def load_best_net(point_type, lead_name):
+    folder_path = "SAVED_NETS"
+    F1_max = 0
+    best_net = ''
+
+    # Проверяем, существует ли папка
+    if not os.path.exists(folder_path):
+        print(f"Папка {folder_path} не существует.")
+        return
+
+    for model_file in os.listdir(folder_path):
+        if model_file.endswith(".pth"):
+            model_path = os.path.join(folder_path, model_file)
+
+            try:
+                model = torch.load(model_path, weights_only=False)
+                F1, _, _, point_type_, lead_name_ = model.get_info()
+                if point_type_ == point_type and lead_name_ == lead_name:
+                    if F1 > F1_max:
+                        F1_max = F1
+                        best_net = model_path
+            except Exception as e:
+                print(f"Ошибка при загрузке или применении модели {model_file}: {e}")
+
+    if not os.path.exists(best_net):
+        raise FileNotFoundError(f"Файл не существует: {best_net}")
+
+    appliable = torch.load(best_net, weights_only=False)
+    return appliable
